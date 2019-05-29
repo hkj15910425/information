044d80103cc21ab5a44608ae93663d87acf23d52
黄凯杰 <hkj15910425@163.com>
2019-05-29 21:00:26 +0800

    首页和详情页的基类模板抽取

---
+++ info/modules/news/views.py
@@ -0,0 +1,17 @@
+from info.modules.news import news_blu
+from flask import render_template
+
+
+@news_blu.route('/<int:news_id>')
+def news_detail(news_id):
+    """
+    新闻详情
+    :param news_id:
+    :return:
+    """
+
+    data = {
+
+    }
+
+    return render_template("news/detail.html", data=data)
